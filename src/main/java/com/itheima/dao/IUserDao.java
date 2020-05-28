b0b6b61f71600f65d0a5389cf7b535db5693900a
yangbin321 <1010545743@qq.com>
2020-05-28 21:24:47 +0800

    day02_eesy_02mybatisCRUD之mybatis的增删查改简单操作

---
+++ src/main/java/com/itheima/dao/IUserDao.java
@@ -0,0 +1,64 @@
+package com.itheima.dao;
+
+import com.itheima.domain.QueryVo;
+import com.itheima.domain.User;
+
+import javax.management.Query;
+import java.util.List;
+
+/**
+ * @author yangbin
+ * @create 2019-09-28 10:52
+ *
+ * 用户的持久层接口
+ *
+ */
+public interface IUserDao {
+
+    /**
+     * 查询所有的方法
+     * @return
+     */
+    List<User> findAll();
+
+    /**
+     * 保存用户的方法
+     * @param user
+     */
+    void saveUser(User user);
+
+    /**
+     * 更新用户的方法
+     * @param user
+     */
+    void updateUser(User user);
+
+    /**
+     * 根据id删除用户的方法
+     */
+    void deleteUser(Integer userId);//注意这里不要写User user，只要一个id就可以删除了
+
+    /**
+     * 根据用户id查询用户信息
+     */
+    User findById(Integer userId);
+
+    /**
+     * 根据名称模糊查询用户信息
+     */
+    List<User> findByName(String username);
+
+    /**
+     * 查询总用户数
+     * @return
+     */
+    int findTotal();
+
+    /**
+     * 根据QueryVo中的条件查询用户
+     * @param vo
+     * @return
+     */
+    List<User> findUserByVo(QueryVo vo);
+
+}
---
+++ src/main/java/com/itheima/domain/QueryVo.java
@@ -0,0 +1,18 @@
+package com.itheima.domain;
+
+/**
+ * @author yangbin
+ * @create 2019-10-01 20:37
+ */
+public class QueryVo {
+
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
